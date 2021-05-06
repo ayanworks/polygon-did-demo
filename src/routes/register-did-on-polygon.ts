a61d6f71e59156a72f47eeec433aecf9bf130d76
Shashank.Kulkarni <shashank.kulkarni@ayanworks.com>
2021-05-06 11:00:17 +0530

    add express module with exposed polygon demo apis

---
+++ src/routes/register-did-on-polygon.ts
@@ -0,0 +1,35 @@
+
+import { registerDID } from '@ayanworks/polygon-did-registrar';
+import * as log4js from "log4js";
+
+const logger = log4js.getLogger();
+logger.level = 'debug';
+
+export class RegisterDid {
+
+    public routes(app): void {
+
+        app.post('/polygon/register-did', async (req, res) => {
+
+            try {
+                const did = req.body.did;
+                const privateKey = req.body.privateKey;
+
+                const registerDidRes = await registerDID(did, privateKey)
+                    .then((response) => {
+                        return response;
+                    });
+
+                res.send(registerDidRes);
+                logger.debug(
+                    `registerDidRes - ${JSON.stringify(registerDidRes)} \n\n\n`
+                );
+            } catch (error) {
+                logger.error(
+                    `RegisterDid Error- ${JSON.stringify(error)} \n\n\n`
+                );
+                res.send(error);
+            }
+        })
+    }
+}
